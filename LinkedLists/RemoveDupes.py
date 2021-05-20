f7a30836d126e656808b4280de83509bb7bf8aa4
StephenDunne97 <dunne134@gmail.com>
2021-05-20 12:10:41 +0100

    Added remove_node method

--- LinkedLists/RemoveDupes.py
+++ LinkedLists/RemoveDupes.py
@@ -8,35 +8,55 @@ Challenge: How would you solve this if an extra buffer was not allowed?
 
 
 # Implementing your own LinkedList from scratch
-class LinkedList: 
-    def __init__(self, nodes = None):
-        self.head = None # Marks the start of the LinkedList
-        if nodes is not None: # If the LinkedList is initiated with data for each node
-            node = Node(data=nodes.pop(0)) # Get first entered node
-            self.head = node # Set first node to head
+class LinkedList:
+    def __init__(self, nodes=None):
+        self.head = None  # Marks the start of the LinkedList
+        if nodes is not None:  # If the LinkedList is initiated with data for each node
+            node = Node(data=nodes.pop(0))  # Get first entered node
+            self.head = node  # Set first node to head
             for elem in nodes:
                 node.next = Node(data=elem)
                 node = node.next
-    
 
-    def __iter__(self): # Iterates over and prints each node in the list. Without this, the "for node in llist_with_data" line will not run.
+    # Iterates over and prints each node in the list. Without this, the "for node in llist_with_data" line will not run.
+    def __iter__(self):
         node = self.head
         while node is not None:
-            yield node # Yield is similar to return
-            node = node.next 
+            yield node  # Yield is similar to return
+            node = node.next
+
+    def remove_node(self, target_node_data):
+        if self.head is None:
+            raise Exception("List is empty")
+
+        if self.head == target_node_data:  # If head is the node to be removed
+            self.head = self.head.next  # The new head is the next node after the head
+            return
+
+        previous_node = self.head
+        for node in self:
+            if node.data == target_node_data:
+                previous_node.next = node.next
+                return
+            previous_node = node
+
+        raise Exception("Node with data '%s' not found." % target_node_data)
+
 
 class Node:
     def __init__(self, data):
-        self.data = data # The data stored in the nde 
-        self.next = None # The next node in the list
-    
-    def __repr__(self): # Special method used to represent a class's objects as a string.
+        self.data = data  # The data stored in the nde
+        self.next = None  # The next node in the list
+
+    # Special method used to represent a class's objects as a string.
+    def __repr__(self):
         return self.data
 
 # Problem: Write code to remove duplicates from an unsorted linked list.
-# Challenge: How would you solve this if an extra buffer was not allowed? 
+# Challenge: How would you solve this if an extra buffer was not allowed?
 
-llist = LinkedList(['f','o','l','l','o','w','u','p',])
+
+llist = LinkedList(['f', 'o', 'l', 'l', 'o', 'w', 'u', 'p', ])
 temp = []
 
 # Using a buffer
@@ -52,4 +72,4 @@ for node in llist:
         llist.remove(node)
 
 
-# Not using a buffer 
+# Not using a buffer
