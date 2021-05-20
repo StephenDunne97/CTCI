583774a05c49f1d567e4eab7a8f551b7ab4e9e8f
StephenDunne97 <dunne134@gmail.com>
2021-05-20 19:50:37 +0100

    Update URLify.py

--- ArraysAndStrings/1.3 - URLify/URLify.py
+++ ArraysAndStrings/1.3 - URLify/URLify.py
@@ -1,12 +1,20 @@
-s = "Hello World I am Stephen"
-r = "%20"
-i = 0
-for x in s:    
-    if x == " ":
-        n = s[0:i] + r + s[i+1:]
-        print(n)
-    i+=1
+def urlify(s): # Long-form solution
+    r = "%20"
+    i = 0
+    for x in s:    
+        if x == " ":
+            n = s[0:i] + r + s[i+1:]
+            print(n)
+        i+=1
+        
+def urlify_pythonic(s): # Same solution done pythonically
+    s = s.replace(" ","%20")
+    print (s)
 
-# Python solution:
-s = s.replace(" ","%20")
-print (s)
+def main():
+    s = "Hello World I am Stephen"
+    urlify(s)
+    urlify_pythonic(s)
+
+if __name__ == "__main__":
+    main()
