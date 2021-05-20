3a13807ab47d9c634574809147a6ac0f9e5c9727
StephenDunne97 <dunne134@gmail.com>
2021-05-20 20:05:26 +0100

    Update StringCompression.py

--- ArraysAndStrings/1.6 - StringCompression/StringCompression.py
+++ ArraysAndStrings/1.6 - StringCompression/StringCompression.py
@@ -1,22 +1,29 @@
-count = 0
-new = ""
-prev = ""
-word = "AAaaBBbbCCccdddddddd"
-st = sorted(word)
-
-for x in st:
+def compress(word):
     count = 0
-    if x == prev:
-        continue
-    else:
-        for y in st:
-            if x == y:
-                count += 1
-                
-    new = new + x + str(count)
-    prev = x
+    new = ""
+    prev = ""
+    st = sorted(word)
 
-if len(new) >= len(st):
-    print(st)
-else:
-    print(new)
+    for x in st:
+        count = 0
+        if x == prev:
+            continue
+        else:
+            for y in st:
+                if x == y:
+                    count += 1
+                    
+        new = new + x + str(count)
+        prev = x
+
+    if len(new) >= len(st):
+        return (st)
+    else:
+        return (new)
+
+
+def main(): 
+    print(compress("AAaaBBbbCCccdddddddd"))
+
+if __name__ == "__main__":
+    main()
