9a5a9ac6075fd0cfc27668611b640087e236c572
StephenDunne97 <dunne134@gmail.com>
2021-09-23 22:26:46 +0100

    Update DivisorDivident.py

--- InterviewQs/DivisorDivident.py
+++ InterviewQs/DivisorDivident.py
@@ -1,31 +1,28 @@
-# Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator. 
-# Return the quotient after dividing dividend by divisor. 
-# The integer division should truncate toward zero, which means losing its fractional part. For example, truncate(8.345) = 8 and truncate(-2.7335) = -2. 
-
-# Example 1: 
-################################################# 
-# Input: dividend = 10, divisor = 3 
-# Output: 3 
-# Explanation: 10/3 = truncate(3.33333..) = 3. 
-
-# Example 2: 
-################################################# 
-# Input: dividend = 7, divisor = -3 
-# Output: -2 
-# Explanation: 7/-3 = truncate(-2.33333..) = -2. 
-
- # Example 3: 
-################################################# 
-
-# Input: dividend = 0, divisor = 1 
-# Output: 0 
-# Example 4: 
-################################################# 
-# Input: dividend = 1, divisor = 1 
-# Output: 1 
-# Constraints: 
-# divisor != 0 
-
+"""
+Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator. 
+Return the quotient after dividing dividend by divisor. 
+The integer division should truncate toward zero, which means losing its fractional part. For example, truncate(8.345) = 8 and truncate(-2.7335) = -2. 
+Example 1: 
+################################################ 
+Input: dividend = 10, divisor = 3 
+Output: 3 
+Explanation: 10/3 = truncate(3.33333..) = 3. 
+Example 2: 
+################################################ 
+Input: dividend = 7, divisor = -3 
+Output: -2 
+Explanation: 7/-3 = truncate(-2.33333..) = -2. 
+ Example 3: 
+################################################ 
+Input: dividend = 0, divisor = 1 
+Output: 0 
+Example 4: 
+################################################ 
+Input: dividend = 1, divisor = 1 
+Output: 1 
+Constraints: 
+divisor != 0 
+"""
 
 def solution(dividend, divisor):
     result = dividend
@@ -46,3 +43,6 @@ def solution(dividend, divisor):
     return (count)
 
 print (solution(1, 1))
+print (solution(10, 3))
+print (solution(7, -3))
+print (solution(0, 1))
